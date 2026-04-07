64d3795ef738f559ee7c0a3d902b6997683a235d
Jonathan Janesjö <jonathan.janesjo@outlook.com>
2026-04-07 23:23:41 +0200

    Initial Commit

---
+++ TestCpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TestCpp
+//
+//  Created by Jonathan Janesjo on 2026-04-07.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
